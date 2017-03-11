c903cb3fd43c467f58cc951d300c3b78948e8e6b
Yves Schelpe <yves.Schelpe@kdg.be>
2017-03-11 12:16:28 +0100

    Added angularjs 1.6.2

---
+++ Module01/PTC/app/product.controller.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app')
+        .controller('ProductController', ProductController);
+
+    function ProductController() {
+        var vm = this;
+
+        vm.product = {
+            productId: 1,
+            productName: 'Video Training'
+        };
+    }
+})();
