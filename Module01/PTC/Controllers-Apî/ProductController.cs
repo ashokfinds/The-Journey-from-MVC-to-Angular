d428e79d921d7175c3737dd7a0c695325077bdb1
Yves Schelpe <yves.Schelpe@kdg.be>
2017-03-11 12:32:52 +0100

    Configured WebAPI

---
+++ Module01/PTC/Controllers-Apî/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PTC.Controllers_Apî
+{
+    public class ProductController : ApiController
+    {
+        // GET api/<controller>
+        public IHttpActionResult Get()
+        {
+            IHttpActionResult ret = null;
+            var vm = new PTCViewModel();
+
+            vm.Get();
+            if(vm.Products.Count > 0) {
+                ret = Ok(vm.Products);
+            } else {
+                ret = NotFound();
+            }
+
+            return ret;
+        }        
+    }
+}
--- Module01/PTC/Global.asax.cs
+++ Module01/PTC/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -13,8 +15,12 @@ namespace PTC
     protected void Application_Start() {
       AreaRegistration.RegisterAllAreas();
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+      GlobalConfiguration.Configure(WebApiConfig.Register);
       RouteConfig.RegisterRoutes(RouteTable.Routes);
-      BundleConfig.RegisterBundles(BundleTable.Bundles);      
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+      var config = GlobalConfiguration.Configuration;
+      config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
     }
   }
 }
