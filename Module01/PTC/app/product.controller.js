2714bd06a73ee36a1f9271e667fecf37da62ea80
Yves Schelpe <yves.Schelpe@kdg.be>
2017-03-11 12:50:25 +0100

    Converted foreach razer to ng-repeat and work with the WebAPI

--- Module01/PTC/app/product.controller.js
+++ Module01/PTC/app/product.controller.js
@@ -4,12 +4,26 @@
     angular.module('app')
         .controller('ProductController', ProductController);
 
-    function ProductController() {
+    function ProductController($http) {
         var vm = this;
+        var dataService = $http;
 
-        vm.product = {
-            productId: 1,
-            productName: 'Video Training'
-        };
+        vm.products = [];
+
+        productList();
+
+        function productList() {
+            dataService.get("/api/Product")
+                .then(function (result) {
+                    vm.products = result.data;                    
+                }, function (error) {
+                    handleException(error);
+                }
+            );
+        }
+
+        function handleException(error) {
+            alert(error.data.ExceptionMessage);
+        }
     }
 })();
